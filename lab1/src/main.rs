a9a675c28462eabe6ec4ba344dfa625642b8babe
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-01 15:10:00 +0300

    minor changes

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -368,13 +368,9 @@ fn check_cbc(n: u16){
     }
 }
 
-fn decrypt_things(key: &[u8], cc: &[u8], mode: &str){
-    let mut iv = cc.clone();
-    iv = &iv[..16];
-    
-
-    let ct = cc.clone();
-    let mut ct = Vec::from(&ct[16..]);
+fn decrypt_things(key: &[u8], cc: &mut [u8], mode: &str){
+    let iv = &cc[..16];
+    let mut ct = Vec::from(&cc[16..]);
 
     let mut c = Cipher{
         bc: None,
@@ -494,15 +490,15 @@ fn main() {
 
     //2.5
     let key = hex!("140b41b22a29beb4061bda66b6747e14");
-    let ct =  hex!("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81");
-    decrypt_things(&key, &ct, "CBC");
-    let ct =  hex!("5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253");
-    decrypt_things(&key, &ct, "CBC");
+    let mut ct =  hex!("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81");
+    decrypt_things(&key, &mut ct, "CBC");
+    let mut ct =  hex!("5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253");
+    decrypt_things(&key, &mut ct, "CBC");
     let key = hex!("36f18357be4dbd77f050515c73fcf9f2");
-    let ct =  hex!("69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329");
-    decrypt_things(&key, &ct, "CTR");
-    let ct =  hex!("770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451");
-    decrypt_things(&key, &ct, "CTR");
+    let mut ct =  hex!("69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329");
+    decrypt_things(&key, &mut ct, "CTR");
+    let mut ct =  hex!("770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451");
+    decrypt_things(&key, &mut ct, "CTR");
 
     //3
     let pt = *b"i hate rust i hate rust i hate rust i ha";
