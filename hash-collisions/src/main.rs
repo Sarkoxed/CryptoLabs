406a18f44e5894665a8a0b7d32e862c093913d66
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-04 04:08:31 +0300

    lsb function birthday attack complete(again) birthday check

--- hash-collisions/src/main.rs
+++ hash-collisions/src/main.rs
@@ -1,8 +1,12 @@
-use sha256::{digest, try_digest};
 use std::collections::HashMap;
+use std::io;
+use std::thread;
+use std::time::{Duration, Instant};
+
 use urandom;
 use hex;
-use std::io;
+
+use sha256::{digest};
 
 fn randbytes(x: &mut [u8]){
     let mut rng = urandom::new();
@@ -11,69 +15,59 @@ fn randbytes(x: &mut [u8]){
     }
 }
 
-fn to_binary(c: char) -> String{
-    match c {
-        '0' => String::from("0000"),
-        '1' => String::from("0001"),
-        '2' => String::from("0010"),
-        '3' => String::from("0011"),
-        '4' => String::from("0100"),
-        '5' => String::from("0101"),
-        '6' => String::from("0110"),
-        '7' => String::from("0111"),
-        '8' => String::from("1000"),
-        '9' => String::from("1001"),
-        'a' => String::from("1010"),
-        'b' => String::from("1011"),
-        'c' => String::from("1100"),
-        'd' => String::from("1101"),
-        'e' => String::from("1110"),
-        'f' => String::from("1111"),
-        _ => String::from(""),
-    }
-}
-
-fn bin(h: String, m: u8) -> String{
-    if ((m + 3) / 4) > h.len() as u8{
+fn lsb(h: &[u8], m: u8) -> String{
+    if ((m + 7) / 8) > h.len() as u8{
         panic!("Wrong number of bits");
     }
 
-    let m_t= (m + 3)/ 4;
-
     let mut res = String::new();
-    let beg = h.len() - m_t as usize;
-
-    for c in &h.as_bytes()[beg..]{
-        let kek = to_binary(*c as char);
-        res = res + &kek;
+    for i in 0..m{
+        let index = (i / 8) as usize;
+        let tmp = (h[h.len() - index - 1] >> (i % 8)) & 1;
+        res = tmp.to_string() + &res;
     }
-    String::from(&res[res.len() - m as usize..])
+    res
 }
 
-
-fn birthday_sha256(m: u8) -> (String, String){
-    let mut S = HashMap::<String, [u8; 16]>::new();
+fn birthday_sha256(m: u8) -> ([u8; 16], [u8; 16]){
+    let mut pairs = HashMap::<String, [u8; 16]>::new();
     loop {
         let mut x = [0u8; 16];
         randbytes(&mut x);
-        let H = bin(digest(&x), m);
 
-        let tmp = S.get(&H);
+        let h = match hex::decode(digest(&x)){
+            Ok(val) => lsb(&val, m), 
+            Err(error) => panic!("{error}"),
+        };
+
+        let tmp = pairs.get(&h);
         _ = match tmp{
-            Some(coll) => return (hex::encode(*coll), hex::encode(x)),
-            None => S.insert(H, x),
+            Some(coll) => return (*coll, x),
+            None => pairs.insert(h, x),
         }
     }
 }    
 
-fn pollard(){}
-
-fn main(){
+fn check_birthday(){
     //let mut index = String::new();
     //io::stdin().read_line(&mut index).expect("Failed to read line");
     //let index: u8 = index.trim().parse().expect("Index entered was not a num");
-    // 
-    //let (x, y) = birthday_sha256(index);
-    //println!("{}", x);
-    //println!("{}", y);
+    for count in 1..30{
+        let now = Instant::now();
+        println!("Current number of bits: {}", count);
+        let (x, y) = birthday_sha256(count);
+        println!("a = {}\nb = {}", hex::encode(x), hex::encode(y));
+        let xt = lsb(&hex::decode(digest(&x)).unwrap(), count);
+        let yt = lsb(&hex::decode(digest(&y)).unwrap(), count);
+        println!("h(a) = {xt}\nh(b) = {yt}");
+        assert_eq!(xt, yt);
+        println!("Time elapsed: {:.10}\n-----------------------", now.elapsed().as_micros() as f64 / 1000000.0);
+    }
+}
+
+fn pollard(){}
+
+fn main(){
+    //check_birthday();
+    println!("{:?}", hex::decode(digest(b"\x85-\x08\xe4XRJ\x1c\x01\xe2\x8fS,\x9e\x19\xb5")).unwrap());
 }
