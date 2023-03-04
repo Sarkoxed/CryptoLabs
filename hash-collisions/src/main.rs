3a0d11c0faaef1e40b71c35a1a64aea2125d1d60
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-04 04:50:45 +0300

    started pollard

--- hash-collisions/src/main.rs
+++ hash-collisions/src/main.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 use std::io;
-use std::thread;
 use std::time::{Duration, Instant};
+use std::thread;
+use std::sync::{Mutex, Arc};
+use std::sync::mpsc;
+
 
 use urandom;
 use hex;
@@ -15,8 +18,8 @@ fn randbytes(x: &mut [u8]){
     }
 }
 
-fn lsb(h: &[u8], m: u8) -> String{
-    if ((m + 7) / 8) > h.len() as u8{
+fn lsb(h: &[u8], m: usize) -> String{
+    if ((m + 7) / 8) > h.len(){
         panic!("Wrong number of bits");
     }
 
@@ -29,14 +32,14 @@ fn lsb(h: &[u8], m: u8) -> String{
     res
 }
 
-fn birthday_sha256(m: u8) -> ([u8; 16], [u8; 16]){
+fn birthday_sha256(m_bits: usize) -> ([u8; 16], [u8; 16]){
     let mut pairs = HashMap::<String, [u8; 16]>::new();
     loop {
         let mut x = [0u8; 16];
         randbytes(&mut x);
 
         let h = match hex::decode(digest(&x)){
-            Ok(val) => lsb(&val, m), 
+            Ok(val) => lsb(&val, m_bits), 
             Err(error) => panic!("{error}"),
         };
 
@@ -65,7 +68,61 @@ fn check_birthday(){
     }
 }
 
-fn pollard(){}
+
+fn pollard_sha256(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_bits: usize
+    if n_threads <= 1{
+        panic!("Not enough threads");
+    }
+
+    let mut pairs = Arc::new(Mutex::new(HashMap::<String, [u8; 16]>::new()));
+    let mut handles = vec![];
+    
+    let q = m_bits / 2 - (n_threads as f64).log2().floor() as usize;
+
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..n_threads{
+        let pairs = Arc::clone(&pairs);
+        let txi = tx.clone();
+        let handle = thread::spawn(move || {
+            let states = vec![];
+
+            let mut state = [0u8; 16];
+            randbytes(&mut state);
+            let mut state = Vec::from(state);
+            let mut counter = 0;
+            loop{
+                state.push(0);                                   // TODO: how to store all the dist
+                                                                 // points for all the threads
+                                                                 // complete the channel thing 
+                                                                 // complete the find for the first
+                                                                 // thread
+                                                                 // store or not the number of
+                                                                 // thread
+                let h = match hex::decode(digest(&state[..])){
+                    Ok(val) => lsb(&val, m_bits), 
+                    Err(error) => panic!("{error}"),
+                };
+
+                let mut map = match pairs.lock(){
+                    Ok(val) => val,
+                    Err(error) => panic!("{error}"),
+                };
+
+                let tmp = (*map).get(&h);
+                if let Some(coll) = tmp{
+                    return (*coll, x);
+                }
+                counter += 1;
+            //*pairs.
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles{
+        handle.join().unwrap();
+    }
+}
 
 fn main(){
     //check_birthday();
