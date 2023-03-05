7efda73f8f4f474918450785c4d50d9675e66fcb
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-05 07:35:34 +0300

    rustfmt'd

--- hash-collisions/src/attacks/mod.rs
+++ hash-collisions/src/attacks/mod.rs
@@ -2,9 +2,9 @@ mod birthday;
 pub use birthday::birthday_sha256;
 
 mod pollard;
-pub use pollard::pollard_short;
 pub use pollard::pollard_full;
+pub use pollard::pollard_short;
 
 mod pollard_own;
-pub use pollard_own::pollard_own_short;
 pub use pollard_own::pollard_own_full;
+pub use pollard_own::pollard_own_short;
