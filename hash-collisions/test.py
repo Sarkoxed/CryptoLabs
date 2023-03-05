7a71d6dbda3330f7a27d7afeddab4c79f4a8f63c
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-05 06:49:15 +0300

    ignore dat

--- hash-collisions/test.py
+++ hash-collisions/test.py
@@ -1,9 +1,22 @@
 from hashlib import sha256
 
-a, b = input(), input()
-n = int(input())
+with open("out", "rt") as f:
+    s = f.read().split("\n")[:-1]
 
-assert (
-    bin(int(sha256(bytes.fromhex(a)).hexdigest(), 16))[-n:]
-    == bin(int(sha256(bytes.fromhex(b)).hexdigest(), 16))[-n:]
-)
+s = [s[i:i + 7][1:4] for i in range(0, len(s), 7)]
+
+for i, (x, y, z) in enumerate(s):
+    a = x[4:]
+    b = y[4:]
+    c = z[7:]
+
+    a = sha256(bytes.fromhex(a)).hexdigest()
+    b = sha256(bytes.fromhex(b)).hexdigest()
+    a = int(a, 16) % 2**(i + 1)
+    b = int(b, 16) % 2**(i + 1)
+    c = int(c, 16)
+    
+    try:
+        assert a == b and b == c
+    except:
+        print(i + 1)
