9e18eab7aec8297f1f26975653497d1cbb2b069e
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-05 07:34:32 +0300

    rustfmt

--- hash-collisions/src/attacks/birthday.rs
+++ hash-collisions/src/attacks/birthday.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::tools::{new_hash, randbytes};
 
-pub fn birthday_sha256(m_bits: usize) -> (Vec<u8>, Vec<u8>){
+pub fn birthday_sha256(m_bits: usize) -> (Vec<u8>, Vec<u8>) {
     let mut pairs = HashMap::<Vec<u8>, Vec<u8>>::new();
     loop {
         let mut x = [0u8; 16];
@@ -11,9 +11,9 @@ pub fn birthday_sha256(m_bits: usize) -> (Vec<u8>, Vec<u8>){
 
         let h = new_hash(&x, m_bits);
         let tmp = pairs.get(&h);
-        _ = match tmp{
+        _ = match tmp {
             Some(coll) => return (coll.clone(), x),
             None => pairs.insert(h, x),
         }
     }
-} 
+}
