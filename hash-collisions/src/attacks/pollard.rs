ccaf9e022d2130ea61209d2bd53ddc440f2d6095
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-05 07:34:43 +0300

    Added anti-cycle check Added k byte extention Added Option return Formatted

--- hash-collisions/src/attacks/pollard.rs
+++ hash-collisions/src/attacks/pollard.rs
@@ -1,39 +1,45 @@
-use std::thread;
-use std::sync::{Mutex, Arc};
-use std::sync::mpsc;
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use std::time::Duration;
 
-use crate::tools::{new_hash, hash, randbytes, hsb, lsb};
+use crate::tools::{hash, hsb, lsb, new_hash, randbytes};
 
-fn dist_point(h: &Vec<u8>, q: usize) -> bool{
-    if q == 0{
-        return true
+fn dist_point(h: &Vec<u8>, q: usize) -> bool {
+    if q == 0 {
+        return true;
     }
     let h = hsb(&h, q);
-    for i in h{
-        if i != 0{
-            return false
+    for i in h {
+        if i != 0 {
+            return false;
         }
     }
     true
 }
 
-pub fn pollard_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_bits: usize
-    if n_threads <= 1{
+fn extend(h: &mut Vec<u8>, k: u8) {
+    for _ in 0..k {
+        h.push(0);
+    }
+}
+
+pub fn pollard_short(n_threads: u8, m_bits: usize, k: u8) -> Option<(Vec<u8>, Vec<u8>)> {
+    if n_threads <= 1 {
         panic!("Not enough threads");
     }
 
     let pairs = Arc::new(Mutex::new(HashMap::<Vec<u8>, (u32, u8)>::new()));
     let q = m_bits as i32 / 2 - (n_threads as f64).log2().floor() as i32;
-    let q = if q > 0 {q as usize} else {0 as usize};
+    let q = if q > 0 { q as usize } else { 0 as usize };
 
     let (tx, rx) = mpsc::channel();
     let mut handles = vec![];
 
     let mut inits = vec![];
-    for th in 0..n_threads{
+    for th in 0..n_threads {
         let pairs_f = Arc::clone(&pairs);
         let txi = tx.clone();
 
@@ -43,37 +49,47 @@ pub fn pollard_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k
         inits.push(state.clone());
 
         let handle = thread::spawn(move || {
-            state.push(0);
+            extend(&mut state, k);
             let mut counter: u32 = 1;
-            loop{
-                println!("Thread {}: state = {}, q = {}", th, hex::encode(&state), q);
-                let mut h = new_hash(&state, m_bits); 
-                h.push(0);
-                if dist_point(&h, q){
-                    println!("I am dist point: {:?}, {}, {}, {:?}", &h, counter, th, hex::encode(state));
-                    let mut map = match pairs_f.lock(){
+            loop {
+                if counter % 10000 == 0 {
+                    // anti-cycle
+                    let mut map = match pairs_f.lock() {
                         Ok(val) => val,
                         Err(error) => panic!("{error}"),
                     };
-                    println!("Unlocked {}", th);
-                    
                     let tmp = (*map).get(&vec![]);
-                    if let Some(_prev) = tmp{       // well to stop we have to wait until the dist
-                                                    // point
+                    if let Some(_prev) = tmp {
                         break;
                     }
-                    println!("Passed {}", th);
+                    drop(map);
+                }
+
+                let mut h = new_hash(&state, m_bits);
+                extend(&mut h, k);
+                if dist_point(&h, q) {
+                    let mut map = match pairs_f.lock() {
+                        Ok(val) => val,
+                        Err(error) => panic!("{error}"),
+                    };
+
+                    let tmp = (*map).get(&vec![]);
+                    if let Some(_prev) = tmp {
+                        // well to stop we have to wait until the dist
+                        // point
+                        break;
+                    }
 
                     let tmp = (*map).get(&h);
-                    if let Some(prev) = tmp{
-                        println!("found, {}", th);
+                    if let Some(prev) = tmp {
                         let (c2, th2) = prev;
                         let c2 = *c2;
                         let th2 = *th2;
+                        println!("found, {}, {}, {}", th, counter, c2);
                         (*map).insert(vec![], (0, 0));
                         _ = txi.send((th, counter, th2, c2));
                         break;
-                    }else{
+                    } else {
                         (*map).insert(h.clone(), (counter, th));
                     }
                     drop(map);
@@ -85,78 +101,70 @@ pub fn pollard_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k
         handles.push(handle);
     }
 
-    let (mut th1, mut i1, mut th2, mut i2) = match rx.recv(){
+    let (mut th1, mut i1, mut th2, mut i2) = match rx.recv() {
         Ok(val) => val,
         Err(error) => panic!("{error}"),
     };
-    
-    println!("ending all the threads");
-    for (i, handle) in handles.into_iter().enumerate(){
+
+    for (i, handle) in handles.into_iter().enumerate() {
         println!("{i}");
         handle.join().unwrap();
     }
 
-    if i1 > i2{
+    if i1 > i2 {
         (i1, i2) = (i2, i1);
         (th1, th2) = (th2, th1);
     }
 
     let d = i2 - i1;
-    
-    println!("thread is {}, {}", th1, inits.len());
+
     let mut state1 = (&inits[th1 as usize]).clone();
     let mut state2 = (&inits[th2 as usize]).clone();
 
-    state1.push(0);
-    state2.push(0);
+    extend(&mut state1, k);
+    extend(&mut state2, k);
 
-    println!("Got: state1 = {}, i1 = {}, state2 = {}, i2 = {}, h1 = {}, h2 = {}", hex::encode(&state1), i1, hex::encode(&state2), i2, hex::encode(new_hash(&state1, m_bits)), hex::encode(new_hash(&state2, m_bits)));
-
-//    let mut state1 = hex::decode("47828172365b028798a0adf7f7c4885a").unwrap();
-//    let mut state2 = hex::decode("7d8cb1cf7ac16bb3cc4657d01b7dc09b").unwrap();
-//    let mut i1 = 6;
-//    let mut i2 = 19;
-//    let mut d = i2 - i1;
- 
-    println!("Got: state1 = {}, i1 = {}, state2 = {}, i2 = {}, h1 = {}, h2 = {}", hex::encode(&state1), i1, hex::encode(&state2), i2, hex::encode(new_hash(&state1, m_bits)), hex::encode(new_hash(&state2, m_bits)));
-    for _ in 0..d{
+    for _ in 0..d {
         state2 = new_hash(&state2, m_bits);
-        state2.push(0);
+        extend(&mut state2, k);
     }
 
-    loop{
+    loop {
         let h1 = new_hash(&state1, m_bits);
         let h2 = new_hash(&state2, m_bits);
 
-        if h1 == h2{
+        if h1 == h2 {
             break;
         }
 
         state1 = h1;
         state2 = h2;
-        state1.push(0);
-        state2.push(0);
+        extend(&mut state1, k);
+        extend(&mut state2, k);
     }
-        
+
     assert_eq!(new_hash(&state1, m_bits), new_hash(&state2, m_bits));
-    println!("returning");
-    (state1, state2)
+    if state1 == state2 {
+        return None;
+    }
+    Some((state1, state2))
 }
 
-pub fn pollard_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_bits: usize
-    if n_threads <= 1{
+pub fn pollard_full(n_threads: u8, m_bits: usize, k: u8) -> Option<(Vec<u8>, Vec<u8>)> {
+    // , k_bits: usize
+    if n_threads <= 1 {
         panic!("Not enough threads");
     }
 
     let pairs = Arc::new(Mutex::new(HashMap::<Vec<u8>, (u32, u8)>::new()));
     let q = m_bits as i32 / 2 - (n_threads as f64).log2().floor() as i32;
-    let q = if q > 0 {q as usize} else {0 as usize};
+    let q = if q > 0 { q as usize } else { 0 as usize };
 
     let (tx, rx) = mpsc::channel();
     let mut handles = vec![];
 
     let mut inits = vec![];
-    for th in 0..n_threads{
+    for th in 0..n_threads {
         let pairs_f = Arc::clone(&pairs);
         let txi = tx.clone();
 
@@ -166,37 +174,45 @@ pub fn pollard_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_
         inits.push(state.clone());
 
         let handle = thread::spawn(move || {
-            state.push(0);
+            extend(&mut state, k);
             let mut counter: u32 = 1;
-            loop{
-                println!("Thread {}: state = {}, q = {}", th, hex::encode(&state), q);
-                let mut h = hash(&state); 
-                let h_1 = lsb(&h, m_bits);
-                h.push(0);
-                if dist_point(&h, q){
-                    println!("I am dist point: {:?}, {}, {}", &h, counter, th);
-                    let mut map = match pairs_f.lock(){
+            loop {
+                if counter % 10000 == 0 {
+                    // anti-cycle
+                    let mut map = match pairs_f.lock() {
                         Ok(val) => val,
                         Err(error) => panic!("{error}"),
                     };
-                    println!("Unlocked {}", th);
-                    
                     let tmp = (*map).get(&vec![]);
-                    if let Some(_prev) = tmp{
+                    if let Some(_prev) = tmp {
                         break;
                     }
-                    println!("Passed {}", th);
+                    drop(map);
+                }
+
+                let mut h = hash(&state);
+                let h_1 = lsb(&h, m_bits);
+                extend(&mut h, k);
+                if dist_point(&h, q) {
+                    let mut map = match pairs_f.lock() {
+                        Ok(val) => val,
+                        Err(error) => panic!("{error}"),
+                    };
+
+                    let tmp = (*map).get(&vec![]);
+                    if let Some(_prev) = tmp {
+                        break;
+                    }
 
                     let tmp = (*map).get(&h_1);
-                    if let Some(prev) = tmp{
-                        println!("found, {}", th);
+                    if let Some(prev) = tmp {
                         let (c2, th2) = prev;
                         let c2 = *c2;
                         let th2 = *th2;
                         (*map).insert(vec![], (0, 0));
                         _ = txi.send((th, counter, th2, c2));
                         break;
-                    }else{
+                    } else {
                         (*map).insert(h_1.clone(), (counter, th));
                     }
                     drop(map);
@@ -208,18 +224,17 @@ pub fn pollard_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_
         handles.push(handle);
     }
 
-    let (mut th1, mut i1, mut th2, mut i2) = match rx.recv(){
+    let (mut th1, mut i1, mut th2, mut i2) = match rx.recv() {
         Ok(val) => val,
         Err(error) => panic!("{error}"),
     };
-    
-    println!("ending all the threads");
-    for (i, handle) in handles.into_iter().enumerate(){
+
+    for (i, handle) in handles.into_iter().enumerate() {
         println!("{i}");
         handle.join().unwrap();
     }
 
-    if i1 > i2{
+    if i1 > i2 {
         (i1, i2) = (i2, i1);
         (th1, th2) = (th2, th1);
     }
@@ -228,35 +243,33 @@ pub fn pollard_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_
 
     let mut state1 = (&inits[th1 as usize]).clone();
     let mut state2 = (&inits[th2 as usize]).clone();
-    state1.push(0);
-    state2.push(0);
+    extend(&mut state1, k);
+    extend(&mut state2, k);
 
-    println!("Got: state1 = {}, i1 = {}, state2 = {}, i2 = {}, h1 = {}, h2 = {}", hex::encode(&state1), i1, hex::encode(&state2), i2, hex::encode(new_hash(&state1, m_bits)), hex::encode(new_hash(&state2, m_bits)));
-    for _ in 0..d{
+    for _ in 0..d {
         state2 = hash(&state2);
-        state2.push(0);
+        extend(&mut state2, k);
     }
 
     let mut count = 0;
-    loop{
+    loop {
         let h1 = hash(&state1);
         let h2 = hash(&state2);
 
-        if lsb(&h1, m_bits) == lsb(&h2, m_bits){
+        if lsb(&h1, m_bits) == lsb(&h2, m_bits) {
             break;
         }
 
         state1 = h1;
         state2 = h2;
-        state1.push(0);
-        state2.push(0);
-        count += 1;
-        if count > i1 || count > i2{
-            break;
-        }
+        extend(&mut state1, k);
+        extend(&mut state2, k);
     }
-        
+
     assert_eq!(new_hash(&state1, m_bits), new_hash(&state2, m_bits));
     println!("returning");
-    (state1, state2)
+    if state1 == state2 {
+        return None;
+    }
+    Some((state1, state2))
 }
--- hash-collisions/src/attacks/pollard_own.rs
+++ hash-collisions/src/attacks/pollard_own.rs
@@ -1,38 +1,44 @@
-use std::thread;
-use std::sync::{Mutex, Arc};
-use std::sync::mpsc;
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use std::time::Duration;
 
-use crate::tools::{new_hash, hash, randbytes, hsb, lsb};
+use crate::tools::{hash, hsb, lsb, new_hash, randbytes};
 
-fn dist_point(h: &Vec<u8>, q: usize) -> bool{
-    if q == 0{
-        return true
+fn dist_point(h: &Vec<u8>, q: usize) -> bool {
+    if q == 0 {
+        return true;
     }
     let h = hsb(&h, q);
-    for i in h{
-        if i != 0{
-            return false
+    for i in h {
+        if i != 0 {
+            return false;
         }
     }
     true
 }
 
-pub fn pollard_own_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_bits: usize
-    if n_threads <= 1{
+fn extend(h: &mut Vec<u8>, k: u8) {
+    for _ in 0..k {
+        h.push(0);
+    }
+}
+
+pub fn pollard_own_short(n_threads: u8, m_bits: usize, k: u8) -> Option<(Vec<u8>, Vec<u8>)> {
+    if n_threads <= 1 {
         panic!("Not enough threads");
     }
 
     let pairs = Arc::new(Mutex::new(HashMap::<Vec<u8>, (Vec<u8>, u32)>::new()));
     let q = m_bits as i32 / 2 - (n_threads as f64).log2().floor() as i32;
-    let q = if q > 0 {q as usize} else {0 as usize};
+    let q = if q > 0 { q as usize } else { 0 as usize };
 
     let (tx, rx) = mpsc::channel();
     let mut handles = vec![];
-    
-    for _th in 0..n_threads{
+
+    for th in 0..n_threads {
         let pairs_f = Arc::clone(&pairs);
         let txi = tx.clone();
 
@@ -40,36 +46,39 @@ pub fn pollard_own_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ //
             let mut state = [0u8; 16];
             randbytes(&mut state);
             let mut state = Vec::from(state);
-            state.push(0);
+            extend(&mut state, k);
 
             let mut prev_dist = state.clone();
             let mut prev_counter: u32 = 0;
 
             let mut counter: u32 = 1;
-            loop{
-//                println!("Thread {}: state = {}, q = {}", th, hex::encode(&state), q);
-                let mut h = new_hash(&state, m_bits); 
-                h.push(0);
-                if dist_point(&h, q){
-//                    println!("I am dist point: {:?}, {}, {}", &h, counter, th);
-                    let mut map = match pairs_f.lock(){
+            loop {
+                if counter % 10000 == 0 {
+                    // anti-cycle
+                    let mut map = match pairs_f.lock() {
                         Ok(val) => val,
                         Err(error) => panic!("{error}"),
                     };
-//                    println!("Unlocked {}", th);
                     let tmp = (*map).get(&vec![]);
-                    if let Some(_prev) = tmp{
+                    if let Some(_prev) = tmp {
                         break;
                     }
-//                    println!("Passed {}", th);
+                    drop(map);
+                }
 
+                let mut h = new_hash(&state, m_bits);
+                extend(&mut h, k);
+                if dist_point(&h, q) {
+                    let mut map = match pairs_f.lock() {
+                        Ok(val) => val,
+                        Err(error) => panic!("{error}"),
+                    };
                     let tmp = (*map).get(&h);
-                    if let Some(_prev) = tmp{
-//                        println!("found, {}", th);
+                    if let Some(_prev) = tmp {
                         (*map).insert(vec![], (vec![], 0));
                         _ = txi.send((prev_dist, counter - prev_counter, h.clone()));
                         break;
-                    }else{
+                    } else {
                         (*map).insert(h.clone(), (prev_dist, counter - prev_counter));
                     }
                     prev_dist = h.clone();
@@ -83,18 +92,16 @@ pub fn pollard_own_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ //
         handles.push(handle);
     }
 
-    let (mut state1, i1, h) = match rx.recv(){
+    let (mut state1, i1, h) = match rx.recv() {
         Ok(val) => val,
         Err(error) => panic!("{error}"),
     };
-    
-//    println!("ending all the threads");
-    for (i, handle) in handles.into_iter().enumerate(){
-        //println!("{i}");
+
+    for (i, handle) in handles.into_iter().enumerate() {
         handle.join().unwrap();
     }
 
-    let map = match pairs.lock(){
+    let map = match pairs.lock() {
         Ok(val) => val,
         Err(error) => panic!("{error}"),
     };
@@ -103,37 +110,37 @@ pub fn pollard_own_short(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ //
     let mut state2 = state2.clone();
     let i2 = *i2;
 
-//    println!("Got: state1 = {}, i1 = {}, state2 = {}, i2 = {}, h1 = {}, h2 = {}", hex::encode(&state1), i1, hex::encode(&state2), i2, hex::encode(new_hash(&state1, m_bits)), hex::encode(new_hash(&state2, m_bits)));
- 
-    for _ in 0..i1-1{
+    for _ in 0..i1 - 1 {
         let h1 = new_hash(&state1, m_bits);
         state1 = h1;
-        state1.push(0);
+        extend(&mut state1, k);
     }
-    for _ in 0..i2-1{
+    for _ in 0..i2 - 1 {
         let h2 = new_hash(&state2, m_bits);
         state2 = h2;
-        state2.push(0);
+        extend(&mut state2, k);
     }
 
     assert_eq!(new_hash(&state1, m_bits), new_hash(&state2, m_bits));
-//    println!("returning");
-    (state1, state2)
+    if state1 == state2 {
+        return None;
+    }
+    Some((state1, state2))
 }
 
-pub fn pollard_own_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // , k_bits: usize
-    if n_threads <= 1{
+pub fn pollard_own_full(n_threads: u8, m_bits: usize, k: u8) -> Option<(Vec<u8>, Vec<u8>)> {
+    if n_threads <= 1 {
         panic!("Not enough threads");
     }
 
     let pairs = Arc::new(Mutex::new(HashMap::<Vec<u8>, (Vec<u8>, u32)>::new()));
     let q = m_bits as i32 / 2 - (n_threads as f64).log2().floor() as i32;
-    let q = if q > 0 {q as usize} else {0 as usize};
+    let q = if q > 0 { q as usize } else { 0 as usize };
 
     let (tx, rx) = mpsc::channel();
     let mut handles = vec![];
-    
-    for _th in 0..n_threads{
+
+    for _th in 0..n_threads {
         let pairs_f = Arc::clone(&pairs);
         let txi = tx.clone();
 
@@ -141,39 +148,47 @@ pub fn pollard_own_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // 
             let mut state = [0u8; 16];
             randbytes(&mut state);
             let mut state = Vec::from(state);
-            state.push(0);
+            extend(&mut state, k);
 
             let mut prev_dist = state.clone();
             let mut prev_counter: u32 = 0;
 
             let mut counter: u32 = 1;
-            loop{
-//                println!("Thread {}: state = {}, q = {}", th, hex::encode(&state), q);
-                let mut h = hash(&state);
-                let h_1 = lsb(&h, m_bits);
-                h.push(0);
-
-                if dist_point(&h, q){
- //                   println!("I am dist point: {:?}, {}, {}", &h, counter, th);
-                    let mut map = match pairs_f.lock(){
+            loop {
+                if counter % 10000 == 0 {
+                    // anti-cycle
+                    let mut map = match pairs_f.lock() {
                         Ok(val) => val,
                         Err(error) => panic!("{error}"),
                     };
-//                    println!("Unlocked {}", th);
-                    
                     let tmp = (*map).get(&vec![]);
-                    if let Some(_prev) = tmp{
+                    if let Some(_prev) = tmp {
                         break;
                     }
-//                    println!("Passed {}", th);
+                    drop(map);
+                }
+
+                let mut h = hash(&state);
+                let h_1 = lsb(&h, m_bits);
+                extend(&mut h, k);
+
+                if dist_point(&h, q) {
+                    let mut map = match pairs_f.lock() {
+                        Ok(val) => val,
+                        Err(error) => panic!("{error}"),
+                    };
+
+                    let tmp = (*map).get(&vec![]);
+                    if let Some(_prev) = tmp {
+                        break;
+                    }
 
                     let tmp = (*map).get(&h_1);
-                    if let Some(_prev) = tmp{
-//                        println!("found, {}", th);
+                    if let Some(_prev) = tmp {
                         (*map).insert(vec![], (vec![], 0));
                         _ = txi.send((prev_dist, counter - prev_counter, h_1.clone()));
                         break;
-                    }else{
+                    } else {
                         (*map).insert(h_1.clone(), (prev_dist, counter - prev_counter));
                     }
                     prev_dist = h.clone();
@@ -187,18 +202,17 @@ pub fn pollard_own_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // 
         handles.push(handle);
     }
 
-    let (mut state1, i1, h) = match rx.recv(){
+    let (mut state1, i1, h) = match rx.recv() {
         Ok(val) => val,
         Err(error) => panic!("{error}"),
     };
-    
-//    println!("ending all the threads");
-    for (i, handle) in handles.into_iter().enumerate(){
+
+    for (i, handle) in handles.into_iter().enumerate() {
         println!("{i}");
         handle.join().unwrap();
     }
 
-    let map = match pairs.lock(){
+    let map = match pairs.lock() {
         Ok(val) => val,
         Err(error) => panic!("{error}"),
     };
@@ -207,20 +221,20 @@ pub fn pollard_own_full(n_threads: u8, m_bits: usize) -> (Vec<u8>, Vec<u8>){ // 
     let mut state2 = state2.clone();
     let i2 = *i2;
 
-//    println!("Got: state1 = {}, i1 = {}, state2 = {}, i2 = {}, h1 = {}, h2 = {}", hex::encode(&state1), i1, hex::encode(&state2), i2, hex::encode(new_hash(&state1, m_bits)), hex::encode(new_hash(&state2, m_bits)));
- 
-    for _ in 0..i1-1{
+    for _ in 0..i1 - 1 {
         let h1 = hash(&state1);
         state1 = h1;
-        state1.push(0);
+        extend(&mut state1, k);
     }
-    for _ in 0..i2-1{
+    for _ in 0..i2 - 1 {
         let h2 = hash(&state2);
         state2 = h2;
-        state2.push(0);
+        extend(&mut state2, k);
     }
 
     assert_eq!(new_hash(&state1, m_bits), new_hash(&state2, m_bits));
-//    println!("returning");
-    (state1, state2)
+    if state1 == state2 {
+        return None;
+    }
+    Some((state1, state2))
 }
