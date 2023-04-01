6547b89fb70626bab18abe9b7f1ac5f952a07278
Sarkoxed <sarkoxed2013@yandex.ru>
2023-04-02 01:19:20 +0300

    PBKDF2 test added

--- lab4/src/main.rs
+++ lab4/src/main.rs
@@ -12,7 +12,7 @@ use serde_json::json;
 
 mod lib;
 
-use crate::lib::{HkdfExtract, HkdfExpand};
+use crate::lib::{HkdfExtract, HkdfExpand, PBKDF2};
 
 fn mix(a: &Vec<f64>, b: &Vec<f64>) -> Vec<u8>{
     let mut res: Vec<u8> = vec![];    
@@ -36,7 +36,6 @@ fn hkdf_test(){
     rand::thread_rng().fill_bytes(&mut salt);
 
     let OKM = HkdfExtract(&salt, &data);
-    let OKM = HkdfExtract(&salt, &data);
 
     let mut keys: Vec<Vec<u8>> = vec![vec![]];
     for i in 1..1001{
@@ -54,10 +53,45 @@ fn hkdf_test(){
 
     let mut file = File::create("data/res.json").unwrap();
     file.write_all(j_bits.to_string().as_bytes()).unwrap();
+}
 
+fn pbkdf2_test(){
+    let mut fc = fs::read_to_string("data/passwords.json").expect("Something wrong with json");
+    let jpas: Vec<&str> = serde_json::from_str(&fc).expect("Failed to unpack json");
+
+    let mut passes = Vec::<Vec<u8>>::new();
+    for p in jpas{
+        passes.push(Vec::from(p));
+    }
+
+    let mut salt = vec![0u8; 256];
+    let mut rand = ChaCha20Rng::from_entropy();
+    
+    let mut keys: Vec<Vec<u8>> = vec![];
+    for pass in passes{
+        println!("{:?}", &pass);
+        rand::thread_rng().fill_bytes(&mut salt);
+        keys.push(PBKDF2(&salt, &pass, 10000));
+        println!("{:?}", &salt);
+        println!("{:?}", &keys[0]);
+        panic!("lol");
+    }
+
+    let mut bits = Vec::<u16>::new();
+
+    for k in keys{
+        let mut m: u16 = (k[1] >> 6) as u16;
+        m += (k[0] << 2) as u16;
+        bits.push(m);
+    }
+    let j_bits = json!(bits);
+
+    let mut file = File::create("data/pass_res.json").unwrap();
+    file.write_all(j_bits.to_string().as_bytes()).unwrap();
 
 }
 
 fn main() {
     // hkdf_test();
+    pbkdf2_test();
 }
