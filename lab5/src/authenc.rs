99524325111ec9acd02c5975d5ae4c0251599c53
Sarkoxed <sarkoxed2013@yandex.ru>
2023-04-26 06:21:32 +0300

    done encryption

--- lab5/src/authenc.rs
+++ lab5/src/authenc.rs
@@ -1,13 +1,15 @@
-use hmac::Hmac;
+use hmac::{Hmac, Mac};
+use hmac::digest::KeyInit;
+
 use sha2::Sha256;
+
 use aes::Aes128;
+use aes::cipher::BlockEncrypt;
 use aes::cipher::{
-    BlockCipher, BlockEncrypt, BlockDecrypt, KeyInit,
     generic_array::{GenericArray, typenum::U16},
 };
 
 use rand::RngCore;
-//use rand::Rng;
 use rand_chacha::ChaCha20Rng;
 use rand_chacha::rand_core::SeedableRng;
 
@@ -15,26 +17,26 @@ type Block = U16;
 const BlockSize: usize = 16;
 const HmacSize: usize = 32;
 
-enum Mode{
+pub enum Mode{
     Enc,
     Dec,
 }
 
 pub struct AuthenticEncryptor{
-    mode:       Mode,
-    counter:    Option<Vec<u8>>,
+    pub mode:       Mode,
+    pub counter:    Option<Vec<u8>>,
 
-    hmac:       Option<Hmac::<Sha256>>,
-    cipher:     Option<Aes128>,
+    pub hmac:       Option<Hmac::<Sha256>>,
+    pub cipher:     Option<Aes128>,
 
-    enc_state:  Option<Vec<u8>>,
-    
-    nonce:      Option<Vec<u8>>,
-    result:     Option<Vec<u8>>,
-    mac:        Option<Vec<u8>>,
+    pub enc_state:  Option<Vec<u8>>,
+
+    pub nonce:      Option<Vec<u8>>,
+    pub result:     Option<Vec<u8>>,
+    pub mac:        Option<Vec<u8>>,
 }
 
-fn rand_bytes(n: usize) -> Vec<u8>{
+pub fn rand_bytes(n: usize) -> Vec<u8>{
     let rand = ChaCha20Rng::from_entropy();
     let mut res = vec![0u8; n];
     rand::thread_rng().fill_bytes(&mut res);
@@ -47,24 +49,25 @@ impl AuthenticEncryptor{
             panic!("Incorrect Key Length: {}", key.len());
         }
  
-        let aes_key = &key[BlockSize..];
-        let hmac_key = &key[..BlockSize];
+        let aes_key = &key[..BlockSize];
+        let hmac_key = &key[BlockSize..];
        
         let cip = Aes128::new(GenericArray::from_slice(aes_key));
         self.cipher = Some(cip);
         self.result = Some(vec![]);
 
-        let mut hmac = Hmac::<Sha256>::new_from_slice(&key[..]).unwrap();
+        self.enc_state = Some(vec![]);
+
+        let hmac = Mac::new_from_slice(hmac_key).unwrap();
+        let hmac: Hmac::<Sha256> = hmac;
         self.hmac = Some(hmac);
 
         match self.mode{
             Mode::Enc => {
-                let mut nonce = rand_bytes(BlockSize);
-                nonce[0] = 0;
-        
-                self.counter = Some(Vec::from(nonce));
-                self.nonce = Some(Vec::from(nonce));
-
+                let mut nonce = rand_bytes(BlockSize / 2);    
+                for _ in 0..BlockSize/2{
+                    nonce.push(0);
+                }
                 self.SetNonce(nonce);            
             }
             _ => ()
@@ -77,19 +80,24 @@ impl AuthenticEncryptor{
             Some(_) => panic!("Nonce already exists")
         }
 
-        let mut hmac = self.hmac.expect("No initialized hmac found");
+        let mut hmac: Hmac<Sha256> = self.hmac.clone().expect("No initialized hmac found");
         hmac.update(&nonce);
-        self.counter = Some(nonce);
-        self.nonce = Some(nonce);
+        self.counter = Some(nonce.clone());
+        self.nonce = Some(nonce.clone());
+        self.hmac = Some(hmac);
     }
 
     fn update_counter(&mut self) -> Vec<u8>{
-        let mut counter: Vec<u8> = self.counter.expect("No counter found");
-        let carry: u8 = 1;
+        let mut counter: Vec<u8> = self.counter.clone().expect("No counter found");
+
+        let cip = self.cipher.clone().expect("No cipher found");
+        let mut out = GenericArray::<u8, Block>::clone_from_slice(&counter[..]);
+        cip.encrypt_block(&mut out);
+ 
+        let mut carry: u8 = 1;
         for i in 0..BlockSize{
             if counter[BlockSize - i - 1] == 0xff && carry == 1{
                 counter[BlockSize - i - 1] = 0;
-                carry = 1;
             }
             else{
                 counter[BlockSize - i - 1] += 1;
@@ -101,18 +109,14 @@ impl AuthenticEncryptor{
         if carry == 1{
             panic!("The counter needs to be reset");
         }
-        
-        let cip = self.cipher.expect("No cipher found");
-        let mut data = GenericArray::<u8, Block>::clone_from_slice(&counter[..]);
- 
-        cip.encrypt_block(&data);
         self.counter = Some(counter);
-        Vec::from(&data[..])
+
+        Vec::from(&out[..])
     }
 
 
-    fn ctr(&mut self, dataBlock: &Vec<u8>){
-        let mut enc_state = self.enc_state.expect("No state found");
+    fn ctr(&mut self, dataBlock: &mut Vec<u8>){
+        let mut enc_state = self.enc_state.clone().expect("No state found");
         for i in 0..dataBlock.len(){
             if enc_state.len() == 0{
                 enc_state = self.update_counter();
@@ -124,44 +128,49 @@ impl AuthenticEncryptor{
         self.enc_state = Some(enc_state);
     }
 
-    pub fn AddBlock(&mut self, dataBlock: &Vec<u8>, isFinal: bool){
-        let mut block: Vec<u8>;
-        let mut mac: Vec<u8>;
-        
-        let mut hmac = self.hmac.expect("No initialized hmac found");
+    pub fn AddBlock(&mut self, dataBlock: &mut Vec<u8>, isFinal: bool){
+        let mut hmac = self.hmac.clone().expect("No initialized hmac found");
 
         match self.mode{
             Mode::Enc => {
                 self.ctr(dataBlock);
-                let mut ct: Vec<u8> = self.result.expect("No ciphertext found");
-                ct.append(&mut dataBlock);
+                let mut ct: Vec<u8> = self.result.clone().expect("No ciphertext found");
+                ct.append(&mut dataBlock.clone());
                 self.result = Some(ct);
                 
-                hmac.update(dataBlock.clone());
+                hmac.update(&dataBlock.clone());
                 if isFinal{
-                    self.mac = Some(hmac.finalize());
+                    self.mac = Some(hmac.clone().finalize().into_bytes().to_vec());
                 }
+                self.hmac = Some(hmac);
             }
             Mode::Dec => {
+                let mut block;
+                let mut mac = vec![];
+
                 if isFinal{
                     if dataBlock.len() < HmacSize{
                         panic!("No mac found in block");
                     }
                     let mac = &dataBlock[dataBlock.len()-HmacSize..];
-                    dataBlock = &Vec::from(&dataBlock[..dataBlock.len() - HmacSize]);
+                    block = Vec::from(&dataBlock[..dataBlock.len() - HmacSize]);
                 }
-                hmac.update(dataBlock.clone());
-                self.ctr(dataBlock);
+                else{
+                    block = dataBlock.clone(); 
+                }
+                hmac.update(&block.clone());
+                println!("Updated by: {:?}", &block);
+                self.ctr(&mut block);
 
-                let mut pt: Vec<u8> = self.result.expect("No plaintext found");
-                pt.append(&mut dataBlock);
+                let mut pt: Vec<u8> = self.result.clone().expect("No plaintext found");
+                pt.append(&mut block);
                 self.result = Some(pt);
 
                 if isFinal{
-                    hmac.verify_slice(mac).unwrap();
+                    hmac.clone().verify_slice(&mac).unwrap();
                 }
+                self.hmac = Some(hmac);
             }
-            other => panic!("Not an option")
         }
     }
 
@@ -169,20 +178,20 @@ impl AuthenticEncryptor{
         let mut res = vec![];
         match self.mode{
             Mode::Enc => {
-                self.AddBlock(data, true);
-                let mut nonce = self.nonce.expect("No nonce found");
-                let mut ciphertext = self.result.expect("No ct found");
-                let mut mac = self.mac.expect("No mac found");
+                self.AddBlock(&mut data.clone(), true);
+                let mut nonce = self.nonce.clone().expect("No nonce found");
+                let mut ciphertext = self.result.clone().expect("No ct found");
+                let mut mac = self.mac.clone().expect("No mac found");
                 res.append(&mut nonce);
                 res.append(&mut ciphertext);
                 res.append(&mut mac);
             }
             Mode::Dec => {
-                let nonce = &data[..BlockSize];
-                self.AddBlock(&Vec::from(&data[BlockSize..]), true);
-                let mut plaintext = self.result.expect("No pt found");
+                let nonce = Vec::from(&data[..BlockSize]);
+                self.SetNonce(nonce);
+                self.AddBlock(&mut Vec::from(&data[BlockSize..]), true);
+                res = self.result.clone().expect("No pt found");
             }
-            other => panic!("Not an option!")
         }
         res
     }
--- lab5/src/main.rs
+++ lab5/src/main.rs
@@ -1,7 +1,39 @@
 mod authenc;
 
-use crate::authenc::AuthenticEncryptor;
+use crate::authenc::{AuthenticEncryptor, rand_bytes, Mode};
 
 fn main() {
+    let key = rand_bytes(32);
+    let pt: Vec<u8> = vec![72, 101, 108, 108, 111, 44, 32, 65, 98, 111, 98, 97, 46, 32, 73, 39, 109, 32, 115, 117, 115, 33];
+    let mut authenc = AuthenticEncryptor{
+        mode: Mode::Enc,
+        counter: None,
+        hmac: None,
+        cipher: None,
+        enc_state: None,
+        nonce: None,
+        result: None,
+        mac: None,
+    };
 
+    authenc.SetKey(key.clone());
+    let ct = authenc.ProcessData(&pt);
+    println!("key = {:?}", &key);
+    println!("pt = {:?}", &pt);
+    println!("ct = {:?}", &ct);
+    
+    //let mut authenc = AuthenticEncryptor{
+    //    mode: Mode::Dec,
+    //    counter: None,
+    //    hmac: None,
+    //    cipher: None,
+    //    enc_state: None,
+    //    nonce: None,
+    //    result: None,
+    //    mac: None,
+    //};
+
+    //authenc.SetKey(key.clone());
+    //let dec = authenc.ProcessData(&ct);
+    //assert_eq!(dec, pt);
 }
