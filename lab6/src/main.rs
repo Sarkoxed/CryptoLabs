84837726ea43eb212c925c0fbf4e54a2ce8312db
Sarkoxed <sarkoxed2013@yandex.ru>
2023-05-02 13:14:21 +0300

    finalize

--- lab6/src/main.rs
+++ lab6/src/main.rs
@@ -1,8 +1,6 @@
 mod tools;
-use crate::tools::{rand_bytes, AuthenticEncryptor, Mode, GenKey, sign};
-
 mod sigma;
-use crate::sigma::{Party, init_party, init_ecdsa, sigma};
+use crate::sigma::{init_party, init_ecdsa, sigma, encrypt, decrypt};
 
 fn main() {
     let mut A = init_party(16);
@@ -11,5 +9,12 @@ fn main() {
     init_ecdsa(&mut A);
     init_ecdsa(&mut B);
 
+    A.o_ecdsa_pk = B.ecdsa_pk.clone();
+    B.o_ecdsa_pk = A.ecdsa_pk.clone();
+
     sigma(&mut A, &mut B);
+    let mA: Vec<u8> = vec![65, 98, 111, 98, 97, 32, 115, 97, 121, 115, 32, 104, 101, 108, 108, 111, 32, 116, 111, 32, 66, 111, 97, 98, 97];
+    let ct = encrypt(&A, &mA);
+    let mB = decrypt(&B, &ct);
+    assert_eq!(mA, mB);
 }
